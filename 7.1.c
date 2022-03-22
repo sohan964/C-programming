6e63a504b35b68d5d19de7c2bda264c67f01d347
sohan964 <86511149+sohan964@users.noreply.github.com>
2022-03-22 06:51:28 +0600

    reverse
    that will make reverse any given number

---
+++ 7.1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,i,rev;
+printf ("Enter A Number:");
+scanf("%d",&n);
+rev=0;
+i=0;
+while(n!=0)
+{
+i=n%10;
+rev=rev*10+i;
+n=n/10;
+}
+printf ("Reverse Num :%d",rev);
+return 0;
+}
